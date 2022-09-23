f2386b4a666d6f1312b39120593d390dbe838cb5
larrychen5155210 <z5155210@gmail.com>
2022-09-24 02:26:50 +0800

    update writing

--- products.py
+++ products.py
@@ -15,3 +15,11 @@ print(products)
 
 for product in products:
 	print(product[0], '的價格是', product[1])
+
+with open('products.txt', 'w') as f: # 'w' : 寫入模式
+	for product in products:
+		f.write(product[0] + ',' + product[1] + '\n')
+
+with open('products.csv', 'w') as f: 
+	for product in products:
+		f.write(product[0] + ',' + product[1] + '\n')
